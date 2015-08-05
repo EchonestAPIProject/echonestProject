b71b3676467e038d041979e1f8aba20eab1849ce
Ivy <vyvymiao1217@outlook.com>
2015-08-05 16:47:31 -0400

    adding dropdown list in html and js file

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 var app = {};
 
-//search field functions
+//search artist field functions
 
 app.searchArtist = function(){
 	$(".search_form").on("submit", function(e){
 		e.preventDefault();
 		app.searchQuery = $(".search_field").val();
-		app.getGenre(app.searchQuery);
+		
 
 	}); // search_form click fuction end here
 
@@ -26,29 +26,53 @@ app.getGenre = function(query){
 		},
 		success: function(artist){
 			console.log(artist.response.artists[0].genres);
+			app.genreDropdown(artist.response.artists[0].genres);
+			app.genreSelected();
+
 		} //end of success function
+
 
 	}); //ajax to search for inputed artist's genre.
 } //app.getGenre funcion end here 
 
+
+
+//function to return a drop down list of genre based on user's artist search
+app.genreDropdown = function(genreList){
+	$.each(genreList, function(index,item){
+		console.log(item.name);
+		var $listItem = $("<option>");
+		$listItem.text(item.name);
+		$listItem.attr("value", item.name);
+		$(".search_dropdown").append($listItem);
+
+	}); //end of each function
+	app.getGenre(app.searchQuery);
+} // app.henreDropdown end here.
+
+//function to store the value from the drop down list selection
+
+app.genreSelected = function(){
+	$(".search_dropdown").on("change", function(e){
+		e.preventDefault();
+		app.searchQuery = $(this).val();
+		console.log(app.searchQuery);
+
+	});
+
+} // end of app.genreSelected
+
+
 //this function is to check the genre for the A&C artists
 app.clientArtist = function(){
-	var allArtists = ["Absolutely Free","Amy Millan","Andy Kim","BADBADNOTGOOD","Bell Orchestre",
-		"Bishop Morocco","Black English","Bloc Party","Broken Social Scene","Calvin Love",
-		"Charles Spearin","Chikita Violenta","Chilly Gonzales","Cold Specks","Dan Mangan + Blacksmith",
-		"Deer Tick","Eight and a Half","Farao","Feist","Fucked Up","Gold and Youth",
-		"Gord Downie, The Sadies, And The Conquering Sun","Hayden","In The Valley Below"
-		"Jason Collett","Kevin Drew","Los Campesinos!","Lowell","Memphis","Mia Maestro"
-		"Moby","No Joy","Ra Ra Riot","Reuben And The Dark","Rey Pila","Royksopp",
-		"Royksopp and Robyn","Sally Seltmann","Snowblink","Still Life Still",
-		"Tei Shi","The Cribs","The Darcys","The Drums","Timber Timbre","Tobias Jesso Jr",
-		"Torres","Tricky","TR/ST","WIN WIN","Years","Zeus","Zulu Winter"
-	]; //end of array of the list of artists name
+
 } //app.clientArtist function ends here
 
 app.init = function(){
 	app.searchArtist();
 	app.getGenre();
+	app.genreDropdown();
+	// app.genreSelected();
 };
 
 $(document).ready(function(){
